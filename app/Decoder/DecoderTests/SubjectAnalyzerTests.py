99b0be137959635d7d0f89025f3c8cbcbde674fc
Alexander Liteplo <alexanderliteplo@gmail.com>
2022-12-22 15:17:18 -0800

    Researching sentence subject analyzer

---
+++ app/Decoder/DecoderTests/SubjectAnalyzerTests.py
@@ -0,0 +1,27 @@
+import unittest
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+from subject_analyzer import SubjectAnalyzer
+
+class TestSubjectAnalyzer(unittest.TestCase):
+    def setUp(self):
+        self.analyzer = SubjectAnalyzer()
+    
+    def test_analyze(self):
+        # Test analyzing a simple sentence
+        result = self.analyzer.analyze("The quick brown fox jumps over the lazy dog.")
+        self.assertEqual(result, "quick brown fox jumps lazy dog ")
+        
+        # Test analyzing a sentence with multiple entities
+        result = self.analyzer.analyze("Barack Obama was the 44th President of the United States.")
+        self.assertEqual(result, "Barack Obama 44th President United States ")
+        
+        # Test analyzing an empty sentence
+        result = self.analyzer.analyze("")
+        self.assertEqual(result, "")
+
+if __name__ == '__main__':
+    unittest.main()
