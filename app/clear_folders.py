1f86e8845151a690fe40599b3ee9b9e00728116b
Alexander Liteplo <alexanderliteplo@gmail.com>
2023-02-20 12:26:52 -0800

    created file for media storage

--- app/clear_folders.py
+++ app/clear_folders.py
@@ -6,20 +6,20 @@ file_deleter = FileDeleter()
 #print working directory
 print("Working directory is: " + os.getcwd())
 
-file_deleter.delete_files_from_folder('./audio_extractions/')
+file_deleter.delete_files_from_folder('./media_storage/audio_extractions/')
 
-file_deleter.delete_files_from_folder('./images/')
+file_deleter.delete_files_from_folder('./media_storage/images/')
 
-file_deleter.delete_files_from_folder('./videos_made_from_images/')
+file_deleter.delete_files_from_folder('./media_storage/videos_made_from_images/')
 
-# file_deleter.delete_files_from_folder('./InputVideos/')
+# file_deleter.delete_files_from_folder('./media_storage/InputVideos/')
 
-# file_deleter.delete_files_from_folder('./OutputVideos/')
+# file_deleter.delete_files_from_folder('./media_storage/OutputVideos/')
 
-file_deleter.delete_files_from_folder('./media_added_videos/')
+file_deleter.delete_files_from_folder('./media_storage/media_added_videos/')
 
-file_deleter.delete_files_from_folder('./resized_original_videos/')
+file_deleter.delete_files_from_folder('./media_storage/resized_original_videos/')
 
-file_deleter.delete_files_from_folder('./videos/')
+file_deleter.delete_files_from_folder('./media_storage/videos/')
 
 print("Files deleted")
